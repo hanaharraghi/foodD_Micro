3a9f25fa536e2e7a6c2d210a6b8b63668bb1536f
harraghi hana <132349622+hanaharraghi@users.noreply.github.com>
2026-04-09 21:33:02 +0100

    Delete DeliveryAndTracking directory

--- DeliveryAndTracking/src/main/java/com/esprit/microservice/deliveryandtrackingservice/Repository/DeliveryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esprit.microservice.deliveryandtrackingservice.Repository;
-
-import com.esprit.microservice.deliveryandtrackingservice.entity.Delivery;
-import org.springframework.data.jpa.repository.JpaRepository;  // ← AJOUTEZ CECI
-
-public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-}
